ac92976d1010c7819aaded92026cbed14677910c
Kinshuk <kinshuk.ram@gmail.com>
2017-04-22 00:09:02 +0200

    Added all the output images.

--- src/final_pipeline.py
+++ src/final_pipeline.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 # Import everything needed to edit/save/watch video clips
 from moviepy.editor import VideoFileClip
-from IPython.display import HTML
+
 
 import src.lane_detector as ldt
 import src.final_thresholder as fth
